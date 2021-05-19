3b6bbf3afbf37be922b14351de208a1e1bbf1a98
ruslanbadaev <r.badaeff@gmail.com>
2021-05-19 23:39:28 +0300

    add token generation

--- scripts/qr-creator.js
+++ scripts/qr-creator.js
@@ -1,8 +1,11 @@
 const publicIp = require("public-ip");
+var jwt = require('jsonwebtoken');
 
 (async () => {
   const tokenData = {
     ip: await publicIp.v4(),
   };
+  const token = jwt.sign(tokenData, 'heh');
   console.log(tokenData);
+  console.log(token);
 })();
