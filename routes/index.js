921757fc44518dcc58f923789b84fb1a52ab9072
ruslanbadaev <r.badaeff@gmail.com>
2021-05-19 23:22:51 +0300

    add bash script

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var exec = require("child_process").exec;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post("/script", function (req, res, next) {
+  console.log(req.query);
+  console.log(req.body);
+  console.log(req.params);
+  exec(req.body.script, function (error, stdout, stderr) {
+    console.log(error);
+
+    if (stderr) res.send({ title: stderr });
+    else res.send({ title: stdout });
+  });
 });
 
 module.exports = router;
