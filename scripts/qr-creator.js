b7c8a9f1404f8310446f6c05646b6ebbe222c2f0
ruslanbadaev <r.badaeff@gmail.com>
2021-05-20 00:04:13 +0300

    add secret input

--- scripts/qr-creator.js
+++ scripts/qr-creator.js
@@ -1,13 +1,27 @@
 const publicIp = require("public-ip");
-const jwt = require('jsonwebtoken');
-const qrcode = require('qrcode-terminal');
+const jwt = require("jsonwebtoken");
+const qrcode = require("qrcode-terminal");
+const { prompt } = require("enquirer");
+const figlet = require('figlet');
 
 (async () => {
+  const secret = await prompt({
+    type: "input",
+    name: "secret",
+    message: "What is your secret word?",
+  });
+
   const tokenData = {
     ip: await publicIp.v4(),
   };
-  const token = jwt.sign(tokenData, 'heh');
-  qrcode.generate(token, {small: true}, function (qrcode) {
-    console.log(qrcode)
-});
+  console.log(figlet.textSync('   Scan it \n with your \nmobile app', {
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}));
+  const token = jwt.sign(tokenData, secret.secret);
+  qrcode.generate(token, { small: true }, function (qrcode) {
+    console.log(qrcode);
+  });
 })();
