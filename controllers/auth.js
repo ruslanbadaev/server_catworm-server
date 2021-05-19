f8caca48a8d11477deb7ba40c29d763280c3c561
ruslanbadaev <r.badaeff@gmail.com>
2021-05-20 01:07:46 +0300

    add auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,8 +3,14 @@ var router = express.Router();
 
 exports.checkToken = async (req, res, next) => {
   try {
+    const token = req.headers.authorization;
+    if (!token) throw { code: 401, message: "UNAUTHORIZED" };
+    const decoded = jwt.verify(token.replace("Bearer ", ""), "wrong-secret");
+    console.log(decoded);
+    req.decoded = decoded;
     next();
   } catch (error) {
-    res.send(error);
+    if (!!error.code) res.status(error.code).send(error.message);
+    else res.status(422).send(error);
   }
 };
