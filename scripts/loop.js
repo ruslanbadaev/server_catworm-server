5fdbfb6067099c59da744577cbdc19b14ea1abf2
ruslanbadaev <r.badaeff@gmail.com>
2021-05-28 00:33:15 +0300

    clear loop file

--- scripts/loop.js
+++ scripts/loop.js
@@ -1,14 +1,5 @@
-const publicIp = require("public-ip");
-const jwt = require("jsonwebtoken");
-const qrcode = require("qrcode-terminal");
-const { prompt } = require("enquirer");
-const storage = require("node-persist");
-
-const crypto = require("crypto");
-const exec = require("child_process").exec;
 const osu = require("node-os-utils");
 const cpu = osu.cpu;
-const osCmd = osu.osCmd;
 const drive = osu.drive;
 const mem = osu.mem;
 
@@ -18,22 +9,10 @@ const mem = osu.mem;
   const memoryMaxPerc = 50;
   try {
     setInterval(async () => {
-      const usage = await cpu.usage();
       const cpuUsage = await cpu.usage();
-      const cpuFree = await cpu.free();
-      const whoami = await osCmd.whoami();
       const driveUsage = await drive.used();
-      const driveFree = await drive.free();
       const memUsage = await mem.used();
-      const memFree = await mem.free();
-      console.log("usage", usage);
-      console.log("cpuUsage", cpuUsage);
-      console.log("cpuFree", cpuFree);
-      console.log("driveUsage", driveUsage);
-      console.log("driveFree", driveFree);
-      console.log("memUsage", memUsage);
-      console.log("memFree", memFree);
-      console.log("whoami", whoami);
+
       const result = {
         cpu: { usage: cpuUsage, total: 100 },
         drive: { usage: driveUsage.usedGb, total: driveUsage.totalGb },
@@ -48,7 +27,6 @@ const mem = osu.mem;
         (Number(memUsage.usedMemMb) / Number(memUsage.totalGb)) * 100 >
           memoryMaxPerc
       ) {
-        console.log("aaaaaaaaaaa");
       }
     }, 2000);
   } catch (error) {
