7e6c6c9bd9bdd8a5d3cabe999b8706cf6b69bf62
ruslanbadaev <r.badaeff@gmail.com>
2021-06-04 00:01:24 +0300

    come back and to json

--- controllers/scripts.js
+++ controllers/scripts.js
@@ -5,11 +5,11 @@ exports.runScript = async (req, res) => {
         // run bash script
         exec(req.body.script, function (error, stdout, err) {
             // send error
-            if (err) res.status(422).send(err);
+            if (err) res.status(422).json({ message: err });
             // send data
-            else res.status(201).send(stdout);
-        });
+            else res.status(201).json({ message: stdout });
+          });
     } catch (error) {
-        res.status(422).send(error);
+        res.status(422).json({ message: error });
     }
-}
+  }
